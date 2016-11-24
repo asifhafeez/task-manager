91f87db61074a9532f1ea447edf318aefc9e4d3e
Malinna Leach <malinnab@yahoo.com>
2016-11-24 18:14:27 +0000

    asynchrony hell

--- client/src/TaskList.js
+++ client/src/TaskList.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import './TaskList.css';
 import Task from './Task';
-import AddTask from './addTask'
+import AddTask from './addTask';
+import superagent from 'superagent';
 
 class TaskList extends Component {
   render() {
     return(
       <div className="TaskList">
       <AddTask addTask={ this.addTask } className="addTask" />
-      { this.renderTasks() }
+      // { this.getTasks() }
       </div>
     );
   }
@@ -17,7 +18,14 @@ class TaskList extends Component {
     super(props);
     this.state =  { tasks: [ "Becoming an awesome dev", "Get paid lots of $$$", "Buy Mary some chocolates" ] };
     this.addTask = this.addTask.bind(this);
+    this.getTasks = this.getTasks.bind(this);
   }
+
+  // showTasks(self) {
+  //   return self.state.tasks.map(description=> (
+  //     <Task key={description} description={description}/>
+  //   ));
+  // }
 
   renderTasks() {
     return this.state.tasks.map(description=> (
@@ -28,6 +36,24 @@ class TaskList extends Component {
   addTask(newDescription) {
     this.setState({ tasks: [...this.state.tasks, newDescription] });
   }
+
+  getTasks() {
+    self = this;
+    superagent.get('/api/tasks')
+    .accept('json')
+    .end(function(err, res) {
+      if(err) throw err;
+      var taskJSON = JSON.parse(res.text);
+      taskJSON.tasks.map(task => (
+        self.addTask(task.description)
+      ));
+      console.log(self.state.tasks);
+      self.renderTasks();
+    });
+  }
+
 }
+
+
 
 export default TaskList;
