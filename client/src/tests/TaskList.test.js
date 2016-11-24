b601e3b767ac06b1d0c30ec9359ca62b4c9f6461
Malinna Leach <malinnab@yahoo.com>
2016-11-24 10:44:29 +0000

    fixed test links

--- client/src/tests/TaskList.test.js
+++ client/src/tests/TaskList.test.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import ReactTestUtils from 'react-addons-test-utils';
 import ReactDOM from 'react-dom';
-import App from './../components/App/App';
-import Task from './../components/App/Task';
-import TaskList from './../components/App/TaskList';
+import App from '../App';
+import Task from '../Task';
+import TaskList from '../TaskList';
 
 
 it("creates a list of task", () => {
